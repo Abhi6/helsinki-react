e7ef381a64be1c2efbda68e29d561516c9996779
Abhi6 <abhinav.mara@gmail.com>
2023-06-22 23:37:19 -0400

    1.7-1.11 completed

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -11,11 +11,49 @@ const Button = (props) => {
 }
 
 // Stats
-const Stats = (props) => {
+const StatisticLine = (props) => {
+	if (props.text === 'positive') {
+		return (
+			<tr>
+				<td>
+					{props.text}
+				</td>
+				<td>
+					{props.value} %
+				</td>
+			</tr>
+		)
+	}
+
 	return (
-		<div>
-			{props.text} {props.value}
-		</div>
+		<tr>
+			<td>
+				{props.text}
+			</td>
+			<td>
+				{props.value}
+			</td>
+		</tr>
+	)
+}
+
+const Statistics = (props) => {
+	if (props.clicks === 0) {
+		return (
+			<div>
+				No feedback given
+			</div>
+		)
+	}
+
+	return (
+		<table>
+			<StatisticLine text={'good'} value={props.good} />
+			<StatisticLine text={'neutral'} value={props.neutral} />
+			<StatisticLine text={'bad'} value={props.bad} />
+			<StatisticLine text={'average'} value={props.average} />
+			<StatisticLine text={'positive'} value={props.positive} />
+		</table>
 	)
 }
 
@@ -23,17 +61,32 @@ const App = () => {
 	const [good, setGood] = useState(0)
 	const [neutral, setNeutral] = useState(0)
 	const [bad, setBad] = useState(0)
+	const [clicks, setClicks] = useState(0)
+
+	const incrementGood = () => {
+		setGood(good + 1)
+		setClicks(clicks + 1)
+	}
+	const incrementNeutral = () => {
+		setNeutral(neutral + 1)
+		setClicks(clicks + 1)
+	}
+	const incrementBad = () => {
+		setBad(bad + 1)
+		setClicks(clicks + 1)
+	}
+
+	const avg = () => (good - bad) / clicks
+	const pos = () => (good / clicks) * 100
 
 	return (
 		<div>
 			<Header text={'give feedback'} />
-			<Button handleClick={() => setGood(good + 1)} text={'good'} />
-			<Button handleClick={() => setNeutral(neutral + 1)} text={'neutral'} />
-			<Button handleClick={() => setBad(bad + 1)} text={'bad'} />
+			<Button handleClick={incrementGood} text={'good'} />
+			<Button handleClick={incrementNeutral} text={'neutral'} />
+			<Button handleClick={incrementBad} text={'bad'} />
 			<Header text={'statistics'} />
-			<Stats text={'good'} value={good} />
-			<Stats text={'neutral'} value={neutral} />
-			<Stats text={'bad'} value={bad} />
+			<Statistics clicks={clicks} good={good} neutral={neutral} bad={bad} average={avg()} positive={pos()} />
 		</div>
 	)
 
